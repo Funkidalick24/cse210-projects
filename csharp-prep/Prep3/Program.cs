845f48ddd7f23e368dc0038ad1929437f7e651ce
funkidalick <faraidandara1@gmail.com>
2024-11-11 13:19:00 +0200

    All current projects for week 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int guess_number =0;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                guess_number = guess_number+1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                guess_number = guess_number+1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guess_number} guesses. ");
+            }
+
+        }                    
     }
 }
